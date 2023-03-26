9019810928282bacdfd68ecfb5eb4fa8ac870d7e
Sajid-Hasan2007 <sajidhasanapp@gmail.com>
2023-03-26 12:32:53 +0600

    Day-3

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,51 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
-          body: Center(
-            child: Container(
-              width: 200,
-              height: 200,
-              decoration: BoxDecoration(
-                color: Colors.amber,
-              ),
-              child: Text("Hello World"),
+          body: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                  ),
+                  child: Text("Hello World"),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                  ),
+                  child: Text("Hello World"),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                  ),
+                  child: Text("Hello World"),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                  ),
+                  child: Text("Hello World"),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                  ),
+                  child: Text("Hello World"),
+                ),
+              ],
             ),
           ),
         ),
